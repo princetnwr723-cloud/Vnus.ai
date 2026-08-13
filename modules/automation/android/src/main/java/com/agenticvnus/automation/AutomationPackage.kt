5192a7b3114d5b12117ae1ab0b081d2eafadc3d8
princetnwr723-cloud <princetnwr723@gmail.com>
2026-08-13 14:44:48 +0530

    Add AutomationPackage for React Native integration

---
+++ modules/automation/android/src/main/java/com/agenticvnus/automation/AutomationPackage.kt
@@ -0,0 +1,16 @@
+package com.agenticvnus.automation
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+class AutomationPackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return listOf(AutomationModule(reactContext))
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return emptyList()
+    }
+}
